a49d0c70d435a59737369811827d32336909528d
Genie Guo <guogenie@gmail.com>
2025-08-15 13:13:32 -0700

    updated calculator ask-for-another

--- JS101/lesson_2/calculator/calculator.js
+++ JS101/lesson_2/calculator/calculator.js
@@ -3,37 +3,65 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
+const config = require('./calculator_messages.json');
+
 const readline = require("readline-sync");
 
 console.log("Welcome to Calculator!");
 
-console.log("What's the first number?");
-
-let number1 = readline.question();
-
-console.log("What's the second number?");
-
-let number2 = readline.question();
-
-console.log(`${number1} ${number2}`);
-
-console.log(
-  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
-);
-
-let operation = readline.question();
-
-let output;
-
-if (operation === "1") {
-  output = Number(number1) + Number(number2);
-} else if (operation === "2") {
-  output = Number(number1) - Number(number2);
-} else if (operation === "3") {
-  output = Number(number1) * Number(number2);
-} else if (operation === "4") {
-  output = Number(number1) / Number(number2);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(output);
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+let another_cal = true;
+
+while (another_cal) {
+  prompt("What's the first number?");
+
+  let number1 = readline.question();
+
+  prompt("What's the second number?");
+
+  let number2 = readline.question();
+
+  prompt(
+    "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
+  );
+
+  let operation = readline.question();
+
+  let output;
+
+  if (operation === "1") {
+    output = Number(number1) + Number(number2);
+  } else if (operation === "2") {
+    output = Number(number1) - Number(number2);
+  } else if (operation === "3") {
+    output = Number(number1) * Number(number2);
+  } else if (operation === "4") {
+    output = Number(number1) / Number(number2);
+  }
+
+  console.log(output);
+
+  console.log("another calculation? y/n")
+
+  switch (readline.question()[0].toLowerCase()){
+    case "y":
+      another_cal = true;
+      break;
+    case "n":
+      another_cal = false;
+      break;
+    default:
+      another_cal = false;
+      break;
+  }
+
+}
+
 
